0d4739cb7f4d2ff61ff2611b7b2dc386dd64641b
ngdangkietse <kietnguyen17052001@gmail.com>
2024-02-29 10:27:58 +0700

    feat: add docker

--- main.go
+++ main.go
@@ -1,5 +1,76 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	println("go-k8s")
+	r := mux.NewRouter()
+	r.Methods(http.MethodGet).Path("/hello").HandlerFunc(handler)
+	r.Methods(http.MethodGet).Path("/health").HandlerFunc(healthHandler)
+	r.Methods(http.MethodGet).Path("/readiness").HandlerFunc(readinessHandler)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Println("Starting server...")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	waitForShutdown(srv)
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		return
+	}
+
+	log.Println("Shutting down")
+	os.Exit(0)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "World"
+	}
+	log.Printf("Received request for name: %s\n", name)
+	_, err := w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
